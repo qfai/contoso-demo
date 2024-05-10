fcd23b9c79ac3159360449f0f7af2a1ce649f2fc
Ke Qu <quke@microsoft.com>
2024-05-10 14:24:58 +0800

    scale one site for qa stage and one site for prod stage

---
+++ prod/prodsite/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "iacdemo"
+    storage_account_name = "contosobackend"
+    container_name       = "backend"
+    key                  = "prodsite.tfstate"
+  }
+}
